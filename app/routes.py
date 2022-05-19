f1ad72d1c36b75bbb67f249c87d917ee44328c06
Maheshkumar P <m.palanisamy@saama.com>
2022-05-19 10:09:12 +0530

    Remove unused import - routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import get_flashed_messages, render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
